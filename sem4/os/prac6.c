ca69b4d3e53990b239de0c207fc8ad2560e8aa07
rishabh-j-23 <rjain2311200@gmail.com>
2023-05-31 20:05:20 +0530

    os pracs

--- sem4/os/prac6.c
+++ sem4/os/prac6.c
@@ -1,5 +1,39 @@
+#include <unistd.h>
+#include <string.h>
 #include <stdio.h>
+#define BUFSIZE 100
+#define READ_END 0
+#define WRITE_END 1
+int main()
+{
+    int pd[2]; // two pipe descriptors:pd[0] for reading and pd[1] for writing
+    char buffer[BUFSIZ + 1];
+    int returnstatus;
+    returnstatus = pipe(pd);
 
-int main(){
-    
+    if (returnstatus == -1)
+    {
+        printf("Unable to create pipe\n");
+        return 1;
+    }
+
+    pid_t fork_result;
+    fork_result = fork();
+
+    if (fork_result == 0)
+    { // child process receives data from parent
+        read(pd[READ_END], buffer, BUFSIZ);
+        printf("\nChild Received: %s from the parent.\n", buffer);
+        close(pd[READ_END]);
+    }
+
+    else
+    { // parent process pass string to child
+        char some_data[10];
+        printf("\nEnter some data to be passed to child process:");
+        gets(some_data);
+        write(pd[WRITE_END], some_data, strlen(some_data));
+        close(pd[WRITE_END]);
+    }
+    return 0;
 }
---
+++ sem4/os/prac9.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#define MAXTRACE 20 // Expt. 9 Program for FIFO page replacement algorithm int
+int main()
+{
+    int reference_string[MAXTRACE], page_faults = 0, m, n, s, pages, frames, pf, k = 0;
+    printf("\nEnter Total Number of Pages :\t");
+    scanf("%d", &pages);
+    printf("\nEnter values of Reference String:\n");
+    for (m = 0; m < pages; m++)
+    {
+        printf("Value No. [%d]:\t", m + 1);
+        scanf("%d", &reference_string[m]);
+    }
+    printf("\nEnter Total Number of Frames:\t");
+    {
+        scanf("%d",
+              &frames);
+    }
+    int temp[frames];
+    for (m = 0; m < frames; m++)
+    {
+        temp[m] = -1;
+    }
+    for (pf = 0; pf <
+                 frames;
+         pf++)
+        printf("PF#%-2d\t", pf + 1);
+    for (m = 0; m < pages; m++)
+    {
+        s = 0;
+        for (n = 0; n < frames;
+             n++)
+        {
+            if (reference_string[m] == temp[n])
+            {
+                s++;
+                page_faults--;
+            }
+        }
+        page_faults++;
+        if ((page_faults <= frames) && (s == 0))
+        {
+            temp[k++] = reference_string[m];
+        }
+        else if (s == 0)
+        {
+            temp[(page_faults -
+                  1) %
+                 frames] = reference_string[m];
+        }
+        printf("\n");
+        for (n = 0; n < frames; n++)
+        {
+            printf("%d\t",
+                   temp[n]);
+        }
+    }
+    printf("\nTotal Page Faults:\t%d\n", page_faults);
+    return 0;
+}
