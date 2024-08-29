b12a3ea2fa1fe5cd5ae90eb2b21124f3e66a1ba5
Rishabh Jain <rjain23112002@gmail.com>
2024-08-29 09:25:50 +0530

    minor change

--- sem7/ai/prac4_a-star-8-puzzle.py
+++ sem7/ai/prac4_a-star-8-puzzle.py
@@ -9,7 +9,6 @@ class AStar:
         self.parent_dict = {}
 
     def heuristic(self, state):
-<<<<<<< HEAD
         # Manhattan distance heuristic
         distance = 0
         for i in range(3):
@@ -18,14 +17,12 @@ class AStar:
                     goal_i, goal_j = divmod(state[i][j] - 1, 3)
                     distance += abs(i - goal_i) + abs(j - goal_j)
         return distance
-=======
         misplace = 0
         for i in range(3):
             for j in range(3):
                 if state[i][j] != self.goal_state[i][j]:
                     misplace += 1
         return misplace
->>>>>>> b698fc2c8362dc1e927f7a025cc541752b0953ea
 
     def is_goal(self, state):
         return state == self.goal_state
@@ -64,11 +61,8 @@ class AStar:
     def solve(self):
         self.open_list.append((0, self.initial_state, None))
         while self.open_list:
-<<<<<<< HEAD
             f_score, current_state, parent = heapq.heappop(self.open_list)
-=======
             _, current_state, parent = heapq.heappop(self.open_list)
->>>>>>> b698fc2c8362dc1e927f7a025cc541752b0953ea
             if self.is_goal(current_state):
                 solution_path = []
                 while parent is not None:
@@ -85,21 +79,14 @@ class AStar:
             self.closed_list.add(tuple(map(tuple, current_state)))
             for neighbor in self.get_neighbors(current_state):
                 if tuple(map(tuple, neighbor)) not in self.closed_list:
-<<<<<<< HEAD
                     g_score = f_score + 1
                     h_score = self.heuristic(neighbor)
                     f_score = g_score + h_score
                     self.parent_dict[tuple(map(tuple, neighbor))] = (current_state, parent)
                     heapq.heappush(self.open_list, (f_score, neighbor, tuple(map(tuple, neighbor))))
-        print("No solution found")
-
-=======
-                    self.parent_dict[tuple(map(tuple, neighbor))] = (current_state, parent)
-                    heapq.heappush(self.open_list, (self.heuristic(neighbor) + len(self.open_list), neighbor, tuple(map(tuple, neighbor))))
-        print("No solution found")
+                    print("No solution found")
 
 # Example usage:
->>>>>>> b698fc2c8362dc1e927f7a025cc541752b0953ea
 initial_state = [[1, 2, 3], [4, 0, 6], [7, 5, 8]]
 astar = AStar(initial_state)
 astar.solve()
